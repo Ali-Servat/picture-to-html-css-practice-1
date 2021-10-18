b038ebd31b26e9042a1d60540077358496fff61d
AliServat <aliservat20@gmail.com>
2021-10-18 14:39:03 +0330

    first revision

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector('.hamberger-menu');
 const links = document.querySelector('.nav-links');
+const hero = document.querySelector('.hero');
 
 menu.addEventListener('click', ()=>{
   links.classList.toggle('active'); 
+  hero.classList.toggle('toggled');
 })
